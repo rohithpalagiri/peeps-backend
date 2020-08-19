221fb4f3068ce6da053bdb36995e7b943bf999f9
rohithpalagiri <rohithpalagiri@gmail.com>
2020-08-19 14:08:10 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+
+//Middleware
+app.use(cors())
+app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(requestLogger)
+
+let users = require('./db.json')
+
+const generateId = () => {
+    const maxId = users.length > 0
+        ? Math.max(...users.map(x => x.id))
+        : 0
+    return maxId + 1
+}
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/users', (req, res) => {
+    res.json(users)
+})
+
+app.get('/api/users/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const user = users.find(user => user.id === id)
+
+    if (user) {
+        response.json(user)
+    } else {
+        response.status(404).end()
+    }
+
+})
+
+app.post('/api/users', (request, response) => {
+
+    const user = request.body
+    user.id = generateId()
+
+    users = users.concat(user)
+
+    response.json(user)
+})
+
+app.delete('/api/users/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+app.use(unknownEndpoint)
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
